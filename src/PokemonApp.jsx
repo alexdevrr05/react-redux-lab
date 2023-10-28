475ee7661a9b1dab8e491fb50763da1dabec4f5a
alexdevrr05 <pv190115553@vallarta.tecmm.edu.mx>
2023-10-28 08:51:01 -0600

    feat: pokemonSlice added

---
+++ src/PokemonApp.jsx
@@ -0,0 +1,9 @@
+const PokemonApp = () => {
+  return (
+    <div>
+      <h1>PokemonApp</h1>
+    </div>
+  );
+};
+
+export default PokemonApp;
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+// import App from './App.jsx';
 import { store } from './store';
 import { Provider } from 'react-redux';
 
 import './index.css';
+import PokemonApp from './PokemonApp.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <React.StrictMode>
-      <App />
+      {/* <App /> */}
+      <PokemonApp />
     </React.StrictMode>
   </Provider>
 );
