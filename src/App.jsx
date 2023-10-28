2db2621d5f77642dd405c2f2b022bb3af591fb40
alexdevrr05 <pv190115553@vallarta.tecmm.edu.mx>
2023-10-27 19:10:48 -0600

    feat: decrement and incrementBy actions added on reducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { increment } from './store/slices/counter';
+import { increment, decrement, incrementBy } from './store/slices/counter';
 
 function App() {
   const { counter } = useSelector((state) => state.counter);
@@ -8,8 +8,15 @@ function App() {
   return (
     <>
       <h1>Vite + React</h1>
+      <h2>count is: {counter}</h2>
       <button type='button' onClick={() => dispatch(increment())}>
-        count is: {counter}
+        Increment
+      </button>
+      <button type='button' onClick={() => dispatch(decrement())}>
+        Decrement
+      </button>
+      <button type='button' onClick={() => dispatch(incrementBy(2))}>
+        Increment by 2
       </button>
     </>
   );
