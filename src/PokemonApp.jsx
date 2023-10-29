ff2a849015511383cdb6c7eee866dd56f4e13d70
alexdevrr05 <pv190115553@vallarta.tecmm.edu.mx>
2023-10-29 10:03:04 -0600

    feat: display pokemons results

--- src/PokemonApp.jsx
+++ src/PokemonApp.jsx
@@ -1,9 +1,11 @@
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { getPokemons } from './store/slices/pokemon';
 
 const PokemonApp = () => {
   const dispatch = useDispatch();
+
+  const { isLoading, page, pokemons } = useSelector((state) => state.pokemons);
 
   useEffect(() => {
     dispatch(getPokemons(0));
@@ -12,6 +14,17 @@ const PokemonApp = () => {
   return (
     <div>
       <h1>PokemonApp</h1>
+
+      {isLoading && <span>Loadinig...</span>}
+
+      <ul>
+        {pokemons.map(({ name }) => {
+          return <li key={name}>{name}</li>;
+        })}
+      </ul>
+      <button disabled={isLoading} onClick={() => dispatch(getPokemons(page))}>
+        Next
+      </button>
     </div>
   );
 };
